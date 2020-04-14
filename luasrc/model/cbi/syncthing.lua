55491a507db8be30701110718d2890a4d08c3509
kannychak <kannychak@qq.com>
2020-04-14 15:28:37 +0800

    Update syncthing.lua

--- luasrc/model/cbi/syncthing.lua
+++ luasrc/model/cbi/syncthing.lua
@@ -13,6 +13,7 @@ s.anonymous = true
 s:option(Flag, "enabled", translate("Enable"))
 
 s:option(Value, "port", translate("port")).default = 8384
+s:option(Value, "conf", translate("conf")).default = "/etc/conf/syncconf"
 s.rmempty = true
 
 
